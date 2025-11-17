9e8fa6a0ff5153082f8c33b1e847cd4104afbc68
Rahat Khan <kmrejoantanjim@gmail.com>
2025-11-17 21:58:00 +0600

    created navbar and footer

--- client/src/layouts/Root.jsx
+++ client/src/layouts/Root.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Root = () => {
     return (
         <div>
-            root
-            <Outlet />
+            <Navbar />
+            <main className='min-h-[calc(100vh-50px)]'>
+                <Outlet />
+            </main>
+
+            <Footer />
         </div>
     );
 };
